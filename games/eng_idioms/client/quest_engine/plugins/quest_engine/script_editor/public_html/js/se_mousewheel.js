baacc397e07a13ff93fbff3e5ec3311f59e79bc1
Eugene Batalov <eabatalov89@gmail.com>
2014-03-23 13:12:59 +0400

    script editor: mouse wheel manager

---
+++ games/eng_idioms/client/quest_engine/plugins/quest_engine/script_editor/public_html/js/se_mousewheel.js
@@ -0,0 +1,61 @@
+function MouseWheelManager() {
+	//Make scene not scrollable and etc
+	this.useDefaultWheelEvents = false;	
+}
+
+/*
+ * cb == function(yDelta)
+ * pixiObj - any interactive pixi object
+ * onInteractive should be called after object interactive events were set up
+ * returns true when wnats event to propagate
+ */
+MouseWheelManager.prototype.onInteractive = function(pixiObj, cb) {
+	var thiz = this;
+	var isMouseOnObj = false;
+
+	var objMouseOver = pixiObj.mouseover;
+	pixiObj.mouseover = function(intData) {
+		isMouseOnObj = true;
+		if (objMouseOver) objMouseOver(intData);
+	};
+
+	var objMouseOut = pixiObj.mouseout;
+	pixiObj.mouseout = function(intData) {
+		isMouseOnObj = false;
+		if (objMouseOut) objMouseOut(intData);
+	}
+
+	this.onDocument(function(yDelta) {
+		if (isMouseOnObj)
+			return cb.call(pixiObj, yDelta);
+		else
+			return thiz.useDefaultWheelEvents;
+	});
+};
+
+MouseWheelManager.prototype.onDocument = function (cb) {
+	this.onSelector(document.body, cb);
+};
+
+MouseWheelManager.prototype.onSelector = function(selector, cb) {
+	$(selector).on('DOMMouseScroll mousewheel wheel', function (e) {
+		return cb(e.originalEvent.detail ||
+			e.originalEvent.wheelDelta ||
+			e.originalEvent.wheelDeltaY);
+		/* http://learn.javascript.ru/mousewheel
+		if(e.originalEvent.detail > 0 || e.originalEvent.wheelDelta < 0) {
+			//alternative options for wheelData: wheelDeltaX & wheelDeltaY
+			//scroll down
+			cb();
+		} else {
+			//scroll up
+			cb();
+		}
+		//prevent page fom scrolling
+		return false;*/
+	});
+};
+
+function MouseWheelManagerFactory() {
+	return new MouseWheelManager();
+}
