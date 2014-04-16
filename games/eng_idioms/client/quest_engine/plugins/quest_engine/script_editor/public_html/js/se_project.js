20e63c7efa9d961bb35bb18ad2096632b8cba2c9
Eugene Batalov <eabatalov89@gmail.com>
2014-04-17 01:40:31 +0400

    script editor: extract save/load logic from se service

---
+++ games/eng_idioms/client/quest_engine/plugins/quest_engine/script_editor/public_html/js/se_project.js
@@ -0,0 +1,69 @@
+function SEProjectSaveService(seService, seEventRouter) {
+    this.seService = seService;
+    this.seEvents = seEventRouter.createEP(SE_ROUTER_EP_ADDR.CONTROLS_GROUP);
+    this.seEvents.on(function(msg) {
+        if (msg.name === "PROJECT_SAVE")
+            this.run();
+    }, this);
+};
+
+SEProjectSaveService.prototype.run = function() {
+    var projectSaved = {
+        script : this.seService.script.save(),
+        scriptEditor : this.seService.scriptEditor.save()
+    };
+    projectSavedJSON = JSON.stringify(projectSaved, null, '\t');
+    var scriptAsBlob = new Blob([projectSavedJSON], { type : 'application/json' });
+    saveAs(scriptAsBlob, this.seService.script.getName() + ".json");
+};
+
+function SEProjectOpenService(seService, seEventRouter) {
+    this.seService = seService;
+    this.seEvents = seEventRouter.createEP(SE_ROUTER_EP_ADDR.CONTROLS_GROUP);
+    this.seEvents.on(function(msg) {
+        if (msg.name === "PROJECT_FILE_OPEN")
+            this.run(msg.json);
+    }, this);
+};
+
+SEProjectOpenService.prototype.run = function(projectSavedJSON) {
+    var projectSaved = JSON.parse(projectSavedJSON);
+    this.seService.scriptEditor.delete();
+    this.seService.script.delete();
+    this.seService.script = SEScript.load(projectSaved.script);
+    this.seService.scriptEditor = ScriptEditor.load(
+        this.seService.script,
+        this.seService.seEventRouter,
+        this.seService.mouseWheelManager,
+        projectSaved.scriptEditor
+    );
+};
+
+//Temporal save/load testing method. Used until we have our unit tests.
+function testSaveLoad() {
+    var scriptSavedJSON = JSON.stringify(this.script.save(), null, '\t');
+    var scriptEditorSavedJSON = JSON.stringify(this.scriptEditor.save(), null, '\t');
+    this.scriptEditor.delete();
+    this.script.delete();
+    this.script = SEScript.load(JSON.parse(scriptSavedJSON));
+    this.scriptEditor = ScriptEditor.load(
+        this.script,
+        this.seEventRouter,
+        this.mouseWheelManager,
+        JSON.parse(scriptEditorSavedJSON)
+    );
+    var scriptEditorSavedJSONAfterLoad = JSON.stringify(this.scriptEditor.save(), null, '\t');
+    var scriptSavedJSONAfterLoad = JSON.stringify(this.script.save(), null, '\t');
+
+    if (scriptEditorSavedJSON !== scriptEditorSavedJSONAfterLoad) {
+        console.error("scriptEditorSavedJSON !== scriptEditorSavedJSONAfterLoad");
+        console.log("Before \n" + scriptEditorSavedJSON);
+        console.log("After: \n" + scriptEditorSavedJSONAfterLoad);
+    }
+
+    if (scriptSavedJSON !== scriptSavedJSONAfterLoad) {
+        console.error(scriptSavedJSON !== scriptSavedJSONAfterLoad);
+        console.log("Before \n" + scriptSavedJSON);
+        console.log("After \n" + scriptSavedJSONAfterLoad);
+    }
+};
--- games/eng_idioms/client/quest_engine/plugins/quest_engine/script_editor/public_html/js/se_service.js
+++ games/eng_idioms/client/quest_engine/plugins/quest_engine/script_editor/public_html/js/se_service.js
@@ -5,9 +5,8 @@ function ScriptEditorService(seEventRouter, mouseWheelManager, userInteractionMa
     this.userInteractionManager = userInteractionManager;
     this.mouseWheelManager = mouseWheelManager;
     this.seEventRouter = seEventRouter;
-    this.seEvents = seEventRouter.createEP(SE_ROUTER_EP_ADDR.CONTROLS_GROUP);
-    this.projectSaver = new SEProjectSaver(this);
-    this.projectOpener = new SEProjectOpener(this);
+    this.projectSaver = new SEProjectSaveService(this, seEventRouter);
+    this.projectOpener = new SEProjectOpenService(this, seEventRouter);
 
     //On application bootstrap we always create script editor with default contents
     this.script = new SEScript("story");
@@ -19,74 +18,12 @@ ScriptEditorService.prototype.getSE = function() {
     return this.scriptEditor;
 };
 
-function SEProjectSaver(seService) {
-    this.seService = seService;
-    seService.seEvents.on(function(msg) {
-        if (msg.name === "PROJECT_SAVE")
-            this.run();
-    }, this);
+ScriptEditorService.prototype.newProject = function() {
+    this.scriptEditor.newStage();
 };
 
-SEProjectSaver.prototype.run = function() {
-    var projectSaved = {
-        script : this.seService.script.save(),
-        scriptEditor : this.seService.scriptEditor.save()
-    };
-    projectSavedJSON = JSON.stringify(projectSaved, null, '\t');
-    var scriptAsBlob = new Blob([projectSavedJSON], { type : 'application/json' });
-    saveAs(scriptAsBlob, this.seService.script.getName() + ".json");
-};
-
-function SEProjectOpener(seService) {
-    this.seService = seService;
-    seService.seEvents.on(function(msg) {
-        if (msg.name === "PROJECT_FILE_OPEN")
-            this.run(msg.json);
-    }, this);
-};
-
-SEProjectOpener.prototype.run = function(projectSavedJSON) {
-    var projectSaved = JSON.parse(projectSavedJSON);
-    this.seService.scriptEditor.delete();
-    this.seService.script.delete();
-    this.seService.script = SEScript.load(projectSaved.script);
-    this.seService.scriptEditor = ScriptEditor.load(
-        this.seService.script,
-        this.seService.seEventRouter,
-        this.seService.mouseWheelManager,
-        projectSaved.scriptEditor
-    );
-};
+ScriptEditorService.prototype.testSaveLoad = testSaveLoad;
 
 function ScriptEditorServiceFactory(seEventRouter, mouseWheelManager, userInteractionManager) {
     return new ScriptEditorService(seEventRouter, mouseWheelManager, userInteractionManager);
 }
-
-//Temporal save/load testing method. Used until we have our unit tests.
-ScriptEditorService.prototype.testSaveLoad = function() {
-    var scriptSavedJSON = JSON.stringify(this.script.save(), null, '\t');
-    var scriptEditorSavedJSON = JSON.stringify(this.scriptEditor.save(), null, '\t');
-    this.scriptEditor.delete();
-    this.script.delete();
-    this.script = SEScript.load(JSON.parse(scriptSavedJSON));
-    this.scriptEditor = ScriptEditor.load(
-        this.script,
-        this.seEventRouter,
-        this.mouseWheelManager,
-        JSON.parse(scriptEditorSavedJSON)
-    );
-    var scriptEditorSavedJSONAfterLoad = JSON.stringify(this.scriptEditor.save(), null, '\t');
-    var scriptSavedJSONAfterLoad = JSON.stringify(this.script.save(), null, '\t');
-
-    if (scriptEditorSavedJSON !== scriptEditorSavedJSONAfterLoad) {
-        console.error("scriptEditorSavedJSON !== scriptEditorSavedJSONAfterLoad");
-        console.log("Before \n" + scriptEditorSavedJSON);
-        console.log("After: \n" + scriptEditorSavedJSONAfterLoad);
-    }
-
-    if (scriptSavedJSON !== scriptSavedJSONAfterLoad) {
-        console.error(scriptSavedJSON !== scriptSavedJSONAfterLoad);
-        console.log("Before \n" + scriptSavedJSON);
-        console.log("After \n" + scriptSavedJSONAfterLoad);
-    }
-};
