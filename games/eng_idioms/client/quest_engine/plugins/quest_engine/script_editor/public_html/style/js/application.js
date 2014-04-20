b8443ec07560d3fb0e121751f50747d3b2c215f5
legandy <andylegenki@hotmail.com>
2014-04-20 11:36:59 +0400

    script editor: change <select> style

--- games/eng_idioms/client/quest_engine/plugins/quest_engine/script_editor/public_html/style/js/application.js
+++ games/eng_idioms/client/quest_engine/plugins/quest_engine/script_editor/public_html/style/js/application.js
@@ -1,27 +1,4 @@
-// Some general UI pack related JS
-// Extend JS String with repeat method
-String.prototype.repeat = function(num) {
-  return new Array(num + 1).join(this);
-};
-
-(function($) {
-
-
-  $(function() {
-
-      // Load Boodstrap.select.js
-      $("select[name='id']").selectpicker({ style: 'btn-sm btn-default' });
-      $("select[name='phraseType']").selectpicker({ style: 'btn-sm btn-default' });
-      $("select[name='objToAddId']").selectpicker({ style: 'btn-sm btn-default' });
-      $("select[name='continue']").selectpicker({ style: 'btn-sm btn-default' });
-
-    // NavigationSidebar with Toggle
-    $("#menu-toggle").click(function (e) {
-        e.preventDefault();
-        $("#wrapper").toggleClass("active");
-    });
-
-    // make code pretty
-    window.prettyPrint && prettyPrint();
-  });
-})(jQuery);
+$("#menu-toggle").click(function (e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("active");
+});
