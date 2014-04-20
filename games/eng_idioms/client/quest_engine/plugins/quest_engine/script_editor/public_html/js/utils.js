59d5017be9320ed7713ffe86da4df72cd575fc26
Eugene Batalov <eabatalov89@gmail.com>
2014-04-20 20:01:41 +0400

    script editor: extend utilities library with ObjectMap class
    ObjectMap allows to use reference value types as keys

--- games/eng_idioms/client/quest_engine/plugins/quest_engine/script_editor/public_html/js/utils.js
+++ games/eng_idioms/client/quest_engine/plugins/quest_engine/script_editor/public_html/js/utils.js
@@ -2,3 +2,67 @@ function assert(assertion, msg) {
     if (!assertion)
         throw msg || "assertion failure";
 }
+
+function collectionObjectDelete(ix, obj) {
+    obj.delete();
+};
+
+function collectionListOfObjectsDelete(ix, list) {
+    jQuery.each(list, collectionObjectDelete);
+};
+
+/* ==== ObjectMap ==== */
+//Uses objects reference equality
+function ObjectMap() {
+    this.map = [];
+};
+
+ObjectMap.prototype.put = function(obj, value) {
+    for (var i = 0; i < this.map.length; ++i) {
+        var listObjEntry = this.map[i];
+        if (listObjEntry.key === obj) {
+            listObjEntry.value = value;
+            return;
+        }
+    }
+    this.map.push({ key : obj, value : value });
+};
+
+ObjectMap.prototype.get = function(obj) {
+    for (var i = 0; i < this.map.length; ++i) {
+        var listObjEntry = this.map[i];
+        if (listObjEntry.key === obj)
+            return listObjEntry.value;
+    }
+};
+
+ObjectMap.prototype.remove = function(obj) {
+    for (var i = 0; i < this.map.length; ++i) {
+        var listObjEntry = this.map[i];
+        if (listObjEntry.key === obj) {
+            this.map.splice(i, 1);
+            return;
+        }
+    }
+};
+
+ObjectMap.prototype.contains = function(obj) {
+    for (var i = 0; i < this.map.length; ++i) {
+        var listObjEntry = this.map[i];
+        if (listObjEntry.key === obj) {
+            return true;
+        }
+    }
+    return false;
+};
+
+/* callback: function(obj, value) */
+ObjectMap.prototype.each = function(callback, thiz) {
+    for (var i = 0; i < this.map.length; ++i) {
+        var listObjEntry = this.map[i];
+        if (thiz)
+            obj.call(thiz, listObjEntry.key, listObjEntry.value);
+        else
+            callback(listObjEntry.key, listObjEntry.value);
+    }
+};
