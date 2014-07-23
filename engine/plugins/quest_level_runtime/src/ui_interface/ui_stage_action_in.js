574fe63ed69d389131896534df4e13dee5537e82
Eugene Batalov <eabatalov89@gmail.com>
2014-07-24 00:16:26 +0400

    quest_level_runtime: move ui_action_in to quest event converter to ui_interface dir
    This converter should be placed near its data type.

--- engine/plugins/quest_level_runtime/src/ui_interface/ui_stage_action_in.js
+++ engine/plugins/quest_level_runtime/src/ui_interface/ui_stage_action_in.js
@@ -40,3 +40,54 @@ UIStageActionIn.prototype.getName = function() {
 UIStageActionIn.prototype.toString = function() {
     return JSON.stringify(this, null, '\t');
 };
+
+function uiStageActionInToQuestEvent(action) {
+    var type = null;
+    var props = null;
+
+	switch(action.getActionType()) {
+		case _UI_STAGE_ACTION_IN.ACTION_TYPES.PLAYER_CLICKED:
+			type = _QUEST_EVENTS.OBJECT_CLICKED;
+            props = { id : _QUEST_PLAYER_ID };
+		break;
+		case _UI_STAGE_ACTION_IN.ACTION_TYPES.NPC_CLICKED:
+            type = _QUEST_EVENTS.OBJECT_CLICKED;
+            props = { id : action.getTargetId() };
+		break;
+		case  _UI_STAGE_ACTION_IN.ACTION_TYPES.ANSWER1_CLICKED:
+            type = _QUEST_EVENTS.ANSWER_1_CLICKED;
+		    props = { id : action.getTargetId() };
+		break;
+		case _UI_STAGE_ACTION_IN.ACTION_TYPES.ANSWER2_CLICKED:
+            type = _QUEST_EVENTS.ANSWER_2_CLICKED,
+			props = { id : action.getTargetId() };
+		break;
+		case _UI_STAGE_ACTION_IN.ACTION_TYPES.ANSWER3_CLICKED:
+            type = _QUEST_EVENTS.ANSWER_3_CLICKED,
+			props = { id : action.getTargetId() };
+		break;
+		case _UI_STAGE_ACTION_IN.ACTION_TYPES.ANSWER4_CLICKED:
+            type = _QUEST_EVENTS.ANSWER_4_CLICKED;
+		    props = { id : action.getTargetId() };
+		break;
+		case  _UI_STAGE_ACTION_IN.ACTION_TYPES.CONTINUE:
+            type = _QUEST_EVENTS.CONTINUE;
+		break;
+        case _UI_STAGE_ACTION_IN.ACTION_TYPES.CUSTOM_EVENT:
+            type = _QUEST_EVENTS.CUSTOM_EVENT;
+		    props = { name : action.getName(), id : action.getTargetId() };
+		break;
+        case _UI_STAGE_ACTION_IN.ACTION_TYPES.NEXT:
+            type = _QUEST_EVENTS.NEXT;
+			props = { id : action.getTargetId() };
+        break;
+        case _UI_STAGE_ACTION_IN.ACTION_TYPES.BACK:
+            type = _QUEST_EVENTS.BACK;
+            props = { id : action.getTargetId() };
+        break;
+		default:
+			console.error("Invalid player stage action type type: "
+                + action.getActionType());
+            return null;
+	}
+}
